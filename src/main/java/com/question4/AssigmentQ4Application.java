f3894b43c02a05b02bda5b5179abdb0d7e691c4a
Divya59-Patil <divyapatil200259@gmail.com>
2023-04-06 10:27:02 +0530

    spring resst

---
+++ src/main/java/com/question4/AssigmentQ4Application.java
@@ -0,0 +1,13 @@
+package com.question4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssigmentQ4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssigmentQ4Application.class, args);
+	}
+
+}
